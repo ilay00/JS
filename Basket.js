2a487eb652e9a82610429ac127b65cb72b671f16
ilay00 <agidelio@mail.ru>
2019-05-10 09:49:53 +0500

    10

--- Basket.js
+++ Basket.js
@@ -1,60 +1,15 @@
-let products=[
-{name:'samsung',price:350},
-{name:'Nexi',price:30}
-];
-let Basket ={
-goodlist:[],
-countTotalPrice(){
-  let totalPrice=0;
-  for (var i = 0; i < this.goodlist.length; i++) {
-    totalPrice+=this.goodlist[i].count*this.goodlist[i].price;
-  }
-      document.write('Итог:'+ totalPrice +'R')
-},
+ //<meta charset="UTF-8">
 
-        countTotalNumber()
-        {
-          let sumCount=0;
-          for(let i=0; i < this.goodlist.length; i++){
-            sumCount+=this.goodlist[i].count;
-          }
-          document.write('Товаров в карзине:'+sumCount);
-        },
-        /*putProduct(prod,count){
-          let idx =this.goodlist.findIndex(predicate function(elem){
-            return elem.name===prod.name;
-          });
-          if (ind === -1) {
-              this.goodlist.push(Object.assign(target{},prod));
-              this.goodlist[this.goodlist.length-1].count=count;
-            }else{
-              this.goodlist[idx].count+=count;
-            }
-          }
-        };
-         for(let i=0; i<products.length; i+=2)
-           {
-               Basket.putProduct(products[i],count:1);
-           }
-            for(let i=0; i<products.length; i+=2)
-           {
-               Basket.putProduct(products[i],count:2);
-           }*/
-              // Basket.countTotalNumber();
-               Basket.countTotalPrice();
-             //  Basket.putProduct();
+//<script>
 
 
 
-
-
-
-/*let product = [''];
- let price = [''];     
-  let a = prompt('Введите товар', '');
+let product = ['samsung','filips'];
+ let price = ['2'];     
+  let a = prompt('Р’РІРµРґРёС‚Рµ С‚РѕРІР°СЂ', '');
  
    product.push(a);
-   let b = prompt('Введите цену','');
+   let b = prompt('Р’РІРµРґРёС‚Рµ С†РµРЅСѓ','');
     price.push(b);    
           
       
@@ -69,15 +24,6 @@ function priceSum(arr) {
     }
     return sum;
 }
-        const s = priceSum(price);
-       let msg="basket nul";
-    if (s || product.length==0 )
-     {        product.length
-            s
-             
-     }else{
-           msg
-          }
-//alert('Сумма к оплате:' + '---Пакупки--' + product.length  + '---Цена--' + s)
+   const s = priceSum(price);
 
-*/
+  
